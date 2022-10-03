51d1e229d744d8b32dcee1aa9327a460eb1ebe6d
Alex Johnson <johnsonav1992@gmail.com>
2022-10-03 09:54:35 -0500

    change header logo link to when logged in

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import classes from './Header.module.css'
 import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 import NavBar from './NavBar.jsx'
 import logo from '../../assets/logo.svg'
 
 const Header = () => {
+	const token = useSelector(state => state.auth.token)
+
 	return (
 		<header className={classes['main-header']}>
-			<Link to="/">
+			<Link to={token ? "/dashboard" : "/"}>
 				<div className={classes['title-wrapper']}>
 					<div className={classes['image-wrapper']}>
 						<img className={classes.img} src={logo} alt="bb-logo" />
