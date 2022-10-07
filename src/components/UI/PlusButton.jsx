e14d0212e2a2199e2e8cf1e2445cd063e02f046b
Alex Johnson <johnsonav1992@gmail.com>
2022-10-07 15:15:31 -0500

    added onClick

--- src/components/UI/PlusButton.jsx
+++ src/components/UI/PlusButton.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import plus from '../../assets/plus.svg'
 import classes from './PlusButton.module.css'
 
-const PlusButton = ({ color }) => {
+const PlusButton = ({ color, onClick }) => {
   color = 'orange'
 
   return (
-    <button className={`${classes.btn} ${classes[color]}`}>
+    <button className={`${classes.btn} ${classes[color]}`} onClick={onClick}>
       <img className={classes.plus} src={plus} alt="plus" />
     </button>
   )
