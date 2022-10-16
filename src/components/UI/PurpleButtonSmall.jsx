fa6c93f1a566693d5eccdf882e16eb5ef2ec3d10
Alex Johnson <johnsonav1992@gmail.com>
2022-10-16 12:53:03 -0500

    Started Account Page

--- src/components/UI/PurpleButtonSmall.jsx
+++ src/components/UI/PurpleButtonSmall.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import classes from './PurpleButtonSmall.module.css'
 
-const PurpleButtonSmall = ({ children, type, onClick }) => {
+const PurpleButtonSmall = ({ children, type, onClick, addClass }) => {
 	return (
-		<button className={classes.btn} type={type} onClick={onClick}>
+		<button className={`${classes.btn} ${classes[addClass]}`} type={type} onClick={onClick}>
 			{children}
 		</button>
 	)
--- src/components/user/AccountPage.jsx
+++ src/components/user/AccountPage.jsx
@@ -1,9 +1,49 @@
 import React from 'react'
 
+import BlueButton from '../UI/BlueButton'
+import PurpleButtonSmall from '../UI/PurpleButtonSmall'
+import Card from '../UI/Card'
+import classes from './AccountPage.module.css'
+
 const AccountPage = () => {
-  return (
-    <div>Account Page</div>
-  )
+	return (
+		<main className={classes['account-main']}>
+			<h1>Account</h1>
+			<p>
+				Fill out your profile details below to change information about
+				your account
+			</p>
+			<Card>
+				<form action="submit" className={classes.form}>
+					<div className={classes.group}>
+						<fieldset>
+							<label htmlFor="firstName">First name</label>
+							<input type="text" />
+						</fieldset>
+						<fieldset>
+							<label htmlFor="lastName">Last name</label>
+							<input type="text" />
+						</fieldset>
+					</div>
+					<div className={classes.group}>
+						<fieldset>
+							<label htmlFor="username">Username</label>
+							<input type="text" />
+						</fieldset>
+						<div className={classes['btn-wrapper']}>
+							<BlueButton>Save</BlueButton>
+						</div>
+					</div>
+				</form>
+			</Card>
+			<div className={classes['edit-buttons']}>
+				<BlueButton>Save</BlueButton>
+				<PurpleButtonSmall addClass={'change-pw'}>
+					Change Password
+				</PurpleButtonSmall>
+			</div>
+		</main>
+	)
 }
 
 export default AccountPage
